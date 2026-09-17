000830324765e32597a4bc8ef98137e0401efa50
rodriguessouza31-ai <rodrigues.souza31@escola.pr.gov.br>
2026-09-17 11:18:52 +0000

    aula 17/09

--- clima/java.js
+++ clima/java.js
@@ -8,6 +8,7 @@ async function buscarclima(cidade){
 const botaoBuscar = document.getElementById("btnBuscar");
 const campoCidade = document.getElementById("cidade");
 const divResultado = document.getElementById("resultado");
+const divPrevisao = document.getElementById("previsao");
 
 botaoBuscar.addEventListener("click", async function(){
     const cidade = campoCidade.value;
@@ -18,10 +19,56 @@ botaoBuscar.addEventListener("click", async function(){
 
     const dados = await buscarclima(cidade);
 
-    if (dados.cod === "404"){   /* tem igualdedade me js é com 3 === */
+    async function realizarBusca(clima){
+     const dados = await buscarclima(cidade);
+
+     if (dados.cod == "404"){   /* tem igualdedade me js é com 3 === */
         divResultado.innerHTML = "<p> Cidade não encontrada.</p>";
         return;
     }
+    const Iconeurl = `https://openweathermap.org/img/wn${dados.weather[0].icon}02x.png`   
+
+divResultado.innerHTML = `
+    <div class="card-clima">
+            <h3>${dados.name}</h3>
+          <img src= "${Iconeurl}" alt="${dados.weather[0].description}">
+            <p>${dados.weather[0].description}</p>
+            <p><strong>${dados.main.temp}ºC</strong></p>
+            <p>Sensação: ${dados.main.feels_like}ºC</p>
+        </div>
+`;
+     localStorage.setItem("ultimaCidade", cidade)
+ 
+     const previsao = await buscarPrevisao(cidade);
+     montarPrevisao(cidade);
+
+    }
+
+     function montarPrevisao(previsao){
+    divPrevisao.innerHTML = "";
+      if  (Item.dt_txt.includes("12:00:00")){
+ const data = new Date(item.dt_txt);
+ const diaSemana = data.toDayeString("pt-br", {weekday: "shot"});
+ const Iconeurl = `https://opernweathermap.org/img/wn${item.weather[0].icon}.png`
+
+ divPrevisao += ``
+
+      }
+
+        
+     }
+
+
+
+
+
+        async function buscarPrevisao(cidade){
+             const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
+            const resposta = await fetch(url);
+            const dados = await resposta.json();
+            return dados;
+        }
+    
 
     divResultado.innerHTML = `
         <div class="card-clima">
